c6b56325d5fdf630e28af081fb855278c0a389e0
İrem <s.iremozdemir@gmail.com>
2023-07-17 20:40:16 +0300

    Last Changes Made, Game Finished

--- Assets/Scripts/gameover.cs
+++ Assets/Scripts/gameover.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class gameover : MonoBehaviour
 {
-    public GameObject GameOverScreen;
-
-    public void gameOver()
+    public GameObject FinishingScreen;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameOverScreen.SetActive(true);
+        FinishingScreen.SetActive(true);
     }
-
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 
 }
